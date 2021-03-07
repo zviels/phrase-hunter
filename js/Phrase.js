68e745be6a9f599e8058e2d5ff3da9e26bc2fb60
zviels <59054323+zviels@users.noreply.github.com>
2021-03-07 14:20:20 +0200

    Wrote The "Phrase" Class

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,67 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Phrase.js */
+class Phrase {
+
+    // C'tor
+
+    constructor(phrase) {
+
+        this.phrase = phrase;
+
+    }
+
+    // Methods
+
+    // addPhraseToDisplay
+
+    addPhraseToDisplay() {
+
+        // createLI
+
+        const createLI = (char) => {
+
+            const li = document.createElement('li');
+
+            if (char === ' ')
+                li.className = 'space';
+
+            else
+                li.className = 'hide letter ' + char;
+                
+            li.textContent = char;
+            return li;
+
+        }
+
+        // Add The Phrase To The Gameboard
+
+        const phrase = document.querySelector('#phrase');
+        const ul = phrase.firstElementChild;
+
+        const letters = this.phrase.split('');
+
+        letters.forEach(letter => {
+
+            const li = createLI(letter);
+            ul.appendChild(li);
+            
+        });
+
+    }
+
+    // checkLetter
+
+    checkLetter(letter) {
+
+        return this.phrase.includes(letter);
+
+    }
+
+    // showMatchedLetter
+
+    showMatchedLetter(letter) {
+
+        const matchingElements = document.querySelectorAll('.' + letter);
+        matchingElements.forEach(element => element.classList.replace('hide', 'show'));
+
+    }
+
+}
